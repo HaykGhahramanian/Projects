479561f19fc466ea4aca813d86e2a6cc0bb4192f
Hayk <ghahramanyanhayk5@gmail.com>
2023-07-25 18:40:12 +0400

    Made some changes

--- myapp/config.py
+++ myapp/config.py
@@ -1,5 +1,5 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:12345@localhost:5432/wotp"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:12345@postgres:5432/wotp"
     SECRET_KEY = 'secret-key'
     SECURITY_FRESHNESS_GRACE_PERIOD = 3600
     SECURITY_DEFAULT_REMEMBER_ME = True
